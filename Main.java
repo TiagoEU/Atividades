ea1c1b661a6e09c0e7ed3a940517af1cefa143d6
Chernobyl <43495251+TiagoEU@users.noreply.github.com>
2024-04-15 15:21:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    
+    //Açoes dos botões
+    public static String ACT1 = "ACT1";
+    public static String ACT2 = "ACT2";
+    public static String ACT3 = "ACT3";
+
+    public static void main(String[] args){
+        //Faça a tividde aqui
+
+    }    
+}
